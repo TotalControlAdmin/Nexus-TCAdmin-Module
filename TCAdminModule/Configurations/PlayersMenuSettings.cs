2adcb172c5f3e60f8aa0661b481133c16d6e01b2
Alex Redding <Alexredding180@gmail.com>
2020-04-05 23:58:51 +0100

    Updates

---
+++ TCAdminModule/Configurations/PlayersMenuSettings.cs
@@ -0,0 +1,11 @@
+﻿using DSharpPlus.Entities;
+
+namespace TCAdminModule.Configurations
+{
+    public class PlayersMenuSettings
+    {
+        public DiscordColor Color { get; set; } = DiscordColor.Green;
+
+        public string ThumbnailUrl { get; set; } = "";
+    }
+}
